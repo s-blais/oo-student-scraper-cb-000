12f4150a63f36be6a1a7d723fc967aeec9bb09a2
Stephen Blais <stephen@s-blais.com>
2019-09-16 20:02:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     scraped_students = []
     doc = Nokogiri::HTML(open(index_url))
-    binding.pry
+    #binding.pry
     doc.css(".student-card").each do |card|
       scraped_student = {
         :name => card.css(".student-name").text,
