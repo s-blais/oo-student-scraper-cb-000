fb0df868628391bdc3fa365b6d5acb43827afe44
Stephen Blais <stephen@s-blais.com>
2019-09-16 19:56:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Scraper
 
-  
+
 
   def self.scrape_index_page(index_url)
     scraped_students = []
@@ -18,7 +18,7 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
