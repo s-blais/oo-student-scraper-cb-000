17f935aa15dac93a8feb9bb001c65f547b7c21ae
Stephen Blais <stephen@s-blais.com>
2019-09-16 21:31:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       Student.new
+    end
   end
 
   def add_student_attributes(attributes_hash)
